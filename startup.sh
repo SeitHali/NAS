31ab9ee1c33e65b0d36e200e2eb21ef2a9a90390
roman seithalilev <seithalilev@gmail.com>
2022-01-11 17:50:10 +0300

    add startup.sh script

---
+++ startup.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+xrandr --output HDMI-1 --mode 3440x1440 --rate 75.00 
+sleep 4
+xrandr --output HDMI-1 --mode 3440x1440 --rate 60.00 
+sleep 4
+xrandr --output HDMI-1 --mode 3440x1440 --rate 75.00 
